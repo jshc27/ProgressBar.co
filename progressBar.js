58094fd670b89694a3fd8fcba1d5062980b592fc
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-07-30 17:29:45 -0500

    adding changes files: styles and functions progressBar2

--- progressBar.js
+++ progressBar.js
@@ -2,3 +2,20 @@ const upload = () => {
     const progressBar =  document.querySelector('.progressBar1');
     progressBar.setAttribute('id', 'play-animation');
 }
+
+
+// function move() {
+
+//     var pb2 = document.getElementById("pb2")
+//     var width = 1;
+//     .pb2.getElementById(click, function () {
+//         if ( width >= 10) {
+//             width++
+//         } else {
+//             width++;
+//             pb2.style.width = width + '%';
+//         }
+//     })
+
+
+// }
