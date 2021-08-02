a0d83d0f98e97f71a6e471f2d9f117ef722b0fa3
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-08-02 16:18:27 -0500

    adding changes: delete functions progressBar2

--- progressBar.js
+++ progressBar.js
@@ -4,37 +4,3 @@ const upload = () => {
 }
 
 
-var progressBar2 = document.querySelector('.progressBar2')
-
-var changeProgress = (progressBar2) => {
-    progressBar2.style.width = `${progressBar2}%`;
-}
-
-
-/* change progress after 1 second (only for showcase) */
-setTimeout(() => changeProgress(22), 1000);
-setTimeout(() => changeProgress(45), 2000);
-setTimeout(() => changeProgress(85), 4600);
-setTimeout(() => changeProgress(98), 5266);
-setTimeout(() => changeProgress(100), 8000);
-
-
-
-
-
-
-// function move() {
-
-//     var pb2 = document.getElementById("pb2")
-//     var width = 1;
-//     .pb2.getElementById(click, function () {
-//         if ( width >= 10) {
-//             width++
-//         } else {
-//             width++;
-//             pb2.style.width = width + '%';
-//         }
-//     })
-
-
-// }
