f1edfee96446b8432f874de8613dfc6ec91d56ee
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-08-02 16:49:29 -0500

    adding changes: functions progressBar2 and styles

--- progressBar.js
+++ progressBar.js
@@ -3,4 +3,12 @@ const upload = () => {
     progressBar.setAttribute('id', 'play-animation');
 }
 
+function incrementar() {
 
+    const progressBar2 = document.querySelector('.progressBar2');
+
+
+
+
+    console.log('si, incrementa');
+}
